421020aa994b12a82a9aa6be995aa8c5d5a06895
nuomi1 <nuomi1@qq.com>
2019-01-03 17:45:45 +0800

    feat: AppDelegate support UserNotifications

--- Timer/AppDelegate.swift
+++ Timer/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import SVProgressHUD
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder {
@@ -18,7 +19,52 @@ class AppDelegate: UIResponder {
 
 extension AppDelegate: UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        SVProgressHUD.setMaximumDismissTimeInterval(1)
+        prepareSVProgressHUD()
+        prepareUserNotification()
+        cleanBadgeNumber()
         return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        cleanBadgeNumber()
+    }
+}
+
+// MARK: - SVProgressHUD
+
+extension AppDelegate {
+    func prepareSVProgressHUD() {
+        SVProgressHUD.setMaximumDismissTimeInterval(1)
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        cleanBadgeNumber()
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    func prepareUserNotification() {
+        UNUserNotificationCenter.current().delegate = self
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if let error = error {
+                debug { print(error) }
+                return
+            }
+
+            if !granted {
+                debug { print("禁止通知权限") }
+            }
+        }
+    }
+
+    func cleanBadgeNumber() {
+        UIApplication.shared.applicationIconBadgeNumber = -1
     }
 }
